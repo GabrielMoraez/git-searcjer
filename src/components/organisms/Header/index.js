fd79e2aaa3cf322424d2389c873d7b65330867ca
GabrielMoraez <moraes.madm@gmail.com>
2018-09-17 20:55:18 -0300

    Upload logo and made few changes in header

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import MenuIcon from '@material-ui/icons/Menu';
 
 import styles from './styles';
 // import AppTitle from '../../atoms/AppTitle';
@@ -19,13 +17,11 @@ class Header extends React.Component {
       <div className={classes.root}>
         <AppBar position='static'>
           <Toolbar>
-            <IconButton className={classes.menuButton} color='inherit' aria-label='Menu'>
-              <MenuIcon />
-            </IconButton>
             <Typography variant='title' color='inherit' className={classes.grow}>
-              News
+              <img src='../../../public/img/logo.png' alt='GITSEARCHER' />
             </Typography>
-            <Button color='inherit'>Login</Button>
+            <Button color='inherit'>ENTRAR</Button>
+            <Button color='inherit'>REGISTRAR</Button>
           </Toolbar>
         </AppBar>
       </div>
