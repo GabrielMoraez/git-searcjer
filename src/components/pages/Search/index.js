0255361f0763df93ccf3f16f8472793a73ae4514
Gabriel Moraes <moraes.madm@gmail.com>
2021-06-21 11:03:01 -0300

    Changed eslint config and fixed some things

--- src/components/pages/Search/index.js
+++ src/components/pages/Search/index.js
@@ -1,29 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
-import TemplateHome from '../../templates/TemplateHome';
+import React from 'react'
+import withStyles from '@material-ui/core/styles/withStyles'
+import TemplateHome from '../../templates/TemplateHome'
 
-import RepoCard from '../../molecules/RepoCard';
+import RepoCard from '../../molecules/RepoCard'
 
-import styles from './styles';
+import styles from './styles'
 
 class Search extends React.Component {
-
   render() {
-    const { classes } = this.props;
-
     return (
       <TemplateHome>
         <RepoCard />
       </TemplateHome>
-    );
+    )
   }
 }
 
-
-Search.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-
-export default withStyles(styles)(Search);
+export default withStyles(styles)(Search)
